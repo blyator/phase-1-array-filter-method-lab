b8b75b2bee35301b160c29114b5820bb201934b9
blyator <dmnbilly@gmail.com>
2025-03-13 11:25:28 +0300

    passing complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+function findMatching(drivers, name) {
+  return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(name.toLowerCase())
+  );
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter((driver) =>
+    driver.toLowerCase().includes(name.toLowerCase())
+  );
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(name.toLowerCase())
+  );
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.name.toLowerCase() === name.toLowerCase()
+  );
+}
